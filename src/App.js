9421d65ca1c8679f89d45a134fb1e19cb9f2fffd
tosesivagon <tose.sivagone25@gmail.com>
2026-05-08 08:19:19 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,157 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+const emptyNote = {
+  content: '',
+  author: '',
+};
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+  // - States
+  const [note, setNote] = useState(emptyNote);
+  const [editNote, setEditNote] = useState(null);
+  const [allNotes, setAllNotes] = useState([]);
+
+  // - Functions form inputs
+  function onNoteValueChange(event) {
+    const { name, value } = event.target;
+    setNote(prevNote => {
+      return {
+        ...prevNote,
+        [name]: value,
+      };
+    });
+  }
+
+  function onEditNoteValueChange(event) {
+    const { name, value } = event.target;
+    setEditNote(prevEditNote => {
+      return {
+        ...prevEditNote,
+        [name]: value,
+      };
+    });
+  }
+
+  // - Functions add, edit, delete
+  function onNoteSubmit(event) {
+    event.preventDefault();
+
+    // Add note
+    setAllNotes(prevAllNotes => {
+      const newNote = { ...note };
+      newNote.id = Date.now().toString();
+      return [newNote, ...prevAllNotes];
+    });
+
+    // Clear add form
+    setNote(emptyNote);
+  }
+
+  function onEditNoteSubmit(event) {
+    event.preventDefault();
+
+    // Edit note
+    setAllNotes(prevAllNotes => {
+      return prevAllNotes.map(note => {
+        if (note.id !== editNote.id) return note;
+        return editNote;
+      });
+    });
+
+    // Clear edit form
+    setEditNote(null);
+  }
+
+  function onNoteDelete(noteId) {
+    setAllNotes(prevAllNotes => {
+      return prevAllNotes.filter(note => note.id !== noteId);
+    });
+  }
+
+  // - Elements
+  const allNotesElements = allNotes.map(theNote => {
+    return (
+      <div key={theNote.id} className="app-note">
+        <p>{theNote.content}</p>
+        <h5>{theNote.author}</h5>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <button
+            onClick={() => {
+              setEditNote(theNote);
+            }}
+          >
+            Edit
+          </button>
+          <span> | </span>
+          <button
+            onClick={() => {
+              onNoteDelete(theNote.id);
+            }}
+          >
+            Delete
+          </button>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </div>
+    );
+  });
+
+  let editNoteElement = null;
+  if (!!editNote) {
+    editNoteElement = (
+      <div className="app-edit-note">
+        <form onSubmit={onEditNoteSubmit}>
+          <p>
+            <textarea
+              rows="3"
+              placeholder="บันทึกความในใจ"
+              name="content"
+              value={editNote.content}
+              onChange={onEditNoteValueChange}
+            />
+          </p>
+          <p>
+            <input
+              type="text"
+              placeholder="ลงชื่อ"
+              name="author"
+              value={editNote.author}
+              onChange={onEditNoteValueChange}
+            />
+          </p>
+          <p>
+            <button type="submit">Edit</button>
+          </p>
+        </form>
+      </div>
+    );
+  }
+
+  return (
+    <section className="app-section">
+      <div className="app-container">
+        <h3>สักหน่อยมั้ยหละ</h3>
+        <form onSubmit={onNoteSubmit}>
+          <p className='app-content'>
+            <textarea 
+              rows="3"
+              placeholder="บันทึกความในใจ"
+              name="content"
+              value={note.content}
+              onChange={onNoteValueChange}
+            />
+          </p>
+          <p className='app-author'>
+            <input type="text" placeholder="ลงชื่อ" name="author" value={note.author} onChange={onNoteValueChange} />
+          </p>
+          <p className='app-button'>
+            <button type="submit">Submit</button>
+          </p>
+        </form>
+        <div className="app-notes">{allNotesElements}</div>
+      </div>
+      {editNoteElement}
+    </section>
   );
 }
 
